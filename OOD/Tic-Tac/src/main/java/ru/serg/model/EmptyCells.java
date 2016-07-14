c75300c8a4e62d579cddbc2342385bb0c0d74a4c
mikheyevsv <mikheyevsv@gmail.com>
2016-07-14 16:58:58 +0300

    11

---
+++ OOD/Tic-Tac/src/main/java/ru/serg/model/EmptyCells.java
@@ -0,0 +1,8 @@
+package ru.serg.model;
+
+/**
+ * Created by Serg on 14.07.2016.
+ */
+public class EmptyCells {
+
+}
--- OOD/Tic-Tac/src/main/java/ru/serg/model/Winner.java
+++ OOD/Tic-Tac/src/main/java/ru/serg/model/Winner.java
@@ -1,23 +1,18 @@
 package ru.serg.model;
 
+import java.util.Arrays;
+
 /**
  * Created by Serg on 14.07.2016.
  */
 public class Winner {
 
-  public int[][] board = new int[3][3];
-
-    public boolean isWinner(){
-
-        for (int i = 0; i < board.length; i++) {
-            int count = 0;
-            for (int j = 0; j < board.length; j++) {
-                if(board[i][j] == count){
-                    count++;
-                }
-            }
-            if (count == this.board.length){
+    int []arr = {1,1,1,1};
+    public boolean isRavno(int[] arr){
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i] == arr[i+1]){
                 return true;
+
             }
         }
         return false;
