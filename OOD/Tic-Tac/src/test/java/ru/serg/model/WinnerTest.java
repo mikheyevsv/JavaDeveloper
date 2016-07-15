de9bd92c7162f068c707fde6bcde1d1fb636652a
mikheyevsv <mikheyevsv@gmail.com>
2016-07-15 13:24:30 +0300

    33

--- OOD/Tic-Tac/src/test/java/ru/serg/model/WinnerTest.java
+++ OOD/Tic-Tac/src/test/java/ru/serg/model/WinnerTest.java
@@ -10,15 +10,16 @@ import static org.junit.Assert.*;
  * Created by Serg on 15.07.2016.
  */
 public class WinnerTest {
+    Winner win = new Winner();
     @Test
     public void whenAllValuesAre() throws Exception {
-        Winner win = new Winner();
+
         boolean res = win.isRavno( new int [] {1,1,1,1});
         assertThat(true, is(equalTo(res)));
     }
     @Test
     public void whenAllValuesNotAre() throws Exception {
-        Winner win = new Winner();
+        
         boolean res = win.isRavno( new int [] {1,0,1,1});
         assertThat(false, is(equalTo(res)));
     }
