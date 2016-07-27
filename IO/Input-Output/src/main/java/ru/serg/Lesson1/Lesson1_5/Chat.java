f579a18924497e95af16f86ecdcc7a0c2f58d7f3
mikheyevsv <mikheyevsv@gmail.com>
2016-07-27 13:53:40 +0300

    klhjklj

--- IO/Input-Output/src/main/java/ru/serg/Lesson1/Lesson1_5/Chat.java
+++ IO/Input-Output/src/main/java/ru/serg/Lesson1/Lesson1_5/Chat.java
@@ -2,11 +2,10 @@ package ru.serg.Lesson1.Lesson1_5;
 
 import org.apache.log4j.Logger;
 import ru.serg.Lesson1.Lesson1_5.Configure.ConfigLog;
+import ru.serg.Lesson1.Lesson1_5.Configure.Receive;
 import ru.serg.Lesson1.Lesson1_5.Configure.Resourses;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /**
  * Created by Serg on 25.07.2016.
@@ -18,22 +17,68 @@ public class Chat {
     private static final String STOP = Resourses.GlobalValues.STOP;
     private static final String END = Resourses.GlobalValues.END;
     private static final String CONTINUE = Resourses.GlobalValues.CONTINUE;
-    private static final String PHRASE = Resourses.GlobalValues.PHRASE;
-    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static  String PHRASE = Resourses.GlobalValues.PHRASE;
+    private static  String CHATLOG = Resourses.GlobalValues.CHATLOG;
+    private Receive receive;
+    private Send send;
+    private boolean stop;
+    private boolean stopChat;
 
 
-    private void close(){
-        try {
-            br.close();
-        } catch (IOException e) {
-            log.error("Something failed", e);
-        }log.info("Stream is closeg");
+
+    public Chat(String phrase, String log) {
+        this.receive = new Receive();
+        this.send = new Send(PHRASE);
+        this.stop = false;
+        this.stopChat = false;
+        CHATLOG = log;
+        PHRASE = phrase;
+
+    }
+
+    public void startProgram() throws IOException {
+        String message;
+        File writer = new File(CHATLOG);
+        PrintWriter out = new PrintWriter(writer.getAbsoluteFile());
+        if (!writer.exists()){
+            writer.createNewFile();
+        }
+        send.fill();
+        while (!stop){
+            message = receive.read();
+            out.print(message + "\n");
+            checkMassage(message);
+            if (!stopChat){
+                out.write(send.generator());
+            }
+        }
+        receive.close();
+        out.flush();
+        out.close();
+    }
+
+
+    public void checkMassage(String massage){
+        if(massage.equalsIgnoreCase(STOP)){
+            this.stop = true;
+        }else if (massage.equalsIgnoreCase(CONTINUE)){
+            this.stop = false;
+        }else if(massage.equalsIgnoreCase(END)){
+            this.stop = true;
+            this.stopChat = true;
+
+        }
     }
 
     public static void main(String[] args) {
         ConfigLog configLog = new ConfigLog(LOG_PROPERTIES_FILE);
         configLog.init();
-        Chat cat = new Chat();
-        cat.close();
+        Chat cat = new Chat(PHRASE, CHATLOG);
+        try {
+            cat.startProgram();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- IO/Input-Output/src/test/java/ru/serg/Lesson1/Lesson1_5/SendMassageTest.java
+++ IO/Input-Output/src/test/java/ru/serg/Lesson1/Lesson1_5/SendMassageTest.java
@@ -1,27 +1,22 @@
-package ru.serg.Lesson1.Lesson1_5;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.fest.assertions.api.Assertions.*;
-
-import static org.junit.Assert.*;
-
-/**
- * Created by Serg on 26.07.2016.
- */
-public class SendMassageTest {
-    private static SendMassage send;
-
-    @BeforeClass
-    public static void initialise(){
-        send = new SendMassage();
-    }
-
-    @Test
-    public void sendmassageTest() throws Exception {
-        String mes = "Hello";
-        String res = send.sendmassage();
-        assertThat(res).isIn(mes);
-    }
-
-}
+//package ru.serg.Lesson1.Lesson1_5;
+//
+//import org.junit.BeforeClass;
+//import static org.hamcrest.CoreMatchers.*;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//
+///**
+// * Created by Serg on 26.07.2016.
+// */
+//public class SendMassageTest {
+//    private static SendMassage send;
+//
+//    @BeforeClass
+//    public static void initialise(){
+//        send = new SendMassage();
+//    }
+//
+//
+//
+//}
