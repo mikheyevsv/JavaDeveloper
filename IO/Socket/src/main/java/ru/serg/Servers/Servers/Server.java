1c6182438f267a12e9968232ae08707048622780
mikheyevsv <mikheyevsv@gmail.com>
2016-07-28 17:13:24 +0300

    bbbrr

--- IO/Socket/src/main/java/ru/serg/Servers/Servers/Server.java
+++ IO/Socket/src/main/java/ru/serg/Servers/Servers/Server.java
@@ -6,6 +6,7 @@ import ru.serg.Servers.Configure.Resourses;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 import static ru.serg.Servers.Configure.Resourses.Log.LOG_PROPERTIES_FILE;
 
@@ -24,6 +25,12 @@ public class Server {
     public void createSocket(){
         try{
             ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("I expect a response from the server");
+
+            Socket socket = serverSocket.accept();
+            log.warn("the connection is  not established");
+            System.out.println("the connection is established");
+
         }catch (IOException ie){
             log.error("Chto-to poshlo ne tak", ie);
         }
