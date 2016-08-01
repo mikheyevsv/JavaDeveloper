704516b4d175192d2c00e8a3d575022cee653190
mikheyevsv <mikheyevsv@gmail.com>
2016-08-01 15:53:04 +0300

    gggg

---
+++ IO/Socket/src/main/java/ru/serg/Servers/Receive.java
@@ -0,0 +1,33 @@
+package ru.serg.Servers;
+
+import org.apache.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Serg on 27.07.2016.
+ */
+public class Receive {
+    private static final Logger log = Logger.getLogger(Receive.class);
+    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public String read() {
+        System.out.println("Enter a massage.");
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            log.error("Something failed", e);
+        }
+        return "";
+    }
+
+    public void close(){
+        try {
+            br.close();
+        } catch (IOException e) {
+            log.error("Something failed", e);
+        }log.info("Stream is closeg");
+    }
+}
--- IO/Socket/src/main/java/ru/serg/Servers/Servers/Server.java
+++ IO/Socket/src/main/java/ru/serg/Servers/Servers/Server.java
@@ -3,11 +3,10 @@ package ru.serg.Servers.Servers;
 import org.apache.log4j.Logger;
 import ru.serg.Servers.Configure.ConfigLog;
 import ru.serg.Servers.Configure.Resourses;
+import ru.serg.Servers.Receive;
+import ru.serg.Servers.Send;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -20,12 +19,26 @@ import static ru.serg.Servers.Configure.Resourses.Log.LOG_PROPERTIES_FILE;
 public class Server {
     private static final Logger log = Logger.getLogger(Server.class);
     private static final int PORT = Resourses.GlobalValues.PORT;
+    private static final String STOP = Resourses.GlobalValues.STOP;
+    private static final String END = Resourses.GlobalValues.END;
+    private static final String CONTINUE = Resourses.GlobalValues.CONTINUE;
+    private static  String PHRASE = Resourses.GlobalValues.PHRASE;
+    private static  String CHATLOG = Resourses.GlobalValues.CHATLOG;
+    private Receive receive;
+    private Send send;
+    private boolean stop;
+    private boolean stopChat;
 
     public static void main(String[] args) {
         ConfigLog configLog = new ConfigLog(LOG_PROPERTIES_FILE);
         configLog.init();
         Server server = new Server();
-        server.createSocket();
+
+        try {server.createSocket();
+            server.startProgram();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void createSocket() {
@@ -43,13 +56,45 @@ public class Server {
                     new InputStreamReader(clientSocket.getInputStream()));
 
             String inputLine;
-            while ((inputLine = in.readLine()) != null) {
+            while (true) {
+                inputLine = in.readLine();
                 out.println(inputLine);
+                out.flush();
             }
-            out.close();
-            in.close();
+
         } catch (IOException e) {
             log.error("Chto-to poshlo ne tak", e);
+
+        }
+    }
+
+    public void startProgram() throws IOException {
+        String message;
+        FileWriter writer = new FileWriter(CHATLOG, true);
+        FileOutputStream outputStream = new FileOutputStream(CHATLOG);
+        send.fill();
+        while (!stop){
+            message = receive.read();
+            outputStream.write(message.getBytes());
+            writer.write(message + "\n");
+            checkMassage(message);
+            if (!stopChat){
+                writer.write(send.generator());
+            }
+        }
+        receive.close();
+        outputStream.close();
+        writer.flush();
+        writer.close();
+    }
+    public void checkMassage(String massage){
+        if(massage.equalsIgnoreCase(STOP)){
+            this.stop = true;
+        }else if (massage.equalsIgnoreCase(CONTINUE)){
+            this.stop = false;
+        }else if(massage.equalsIgnoreCase(END)){
+            this.stop = true;
+            this.stopChat = true;
 
         }
     }
