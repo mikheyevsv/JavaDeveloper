8960542148707a8ee24b7ceabd2f6eebbc39a698
mikheyevsv <mikheyevsv@gmail.com>
2016-08-09 17:05:47 +0300

    q

--- IO/SearchFiles/src/main/java/ru/serg/SearchFiles.java
+++ IO/SearchFiles/src/main/java/ru/serg/SearchFiles.java
@@ -10,7 +10,7 @@ import ru.serg.SearchMethod.SearchByRegularExpression;
  * @version 1.0
  */
 public class SearchFiles {
-    
+
     public static void main(String[] args) {
        SearchFiles searchFiles = new SearchFiles();
         searchFiles.startSearch(args);
@@ -19,14 +19,14 @@ public class SearchFiles {
     public void startSearch(String [] args){
         Validator validator = new Validator();
         if(validator.valid(args)) {
-            String findParam = args[1];
-            if(findParam.equals("-m")) {
+            String param = args[2];
+            if(param.equals("-m")) {
                 SearchByMask mask = new SearchByMask();
                 mask.find(args);
-            } else if(findParam.equals("-f")) {
+            } else if(param.equals("-f")) {
                 SearchByFileName name = new SearchByFileName();
                 name.find(args);
-            } else if(findParam.equals("-r")) {
+            } else if(param.equals("-r")) {
                 SearchByRegularExpression regular = new SearchByRegularExpression();
                 regular.find(args);
             }
