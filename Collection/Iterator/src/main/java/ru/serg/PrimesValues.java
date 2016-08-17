b559a870007916068c348a1f9c29614ab102ae8f
mikheyevsv <mikheyevsv@gmail.com>
2016-08-17 16:21:37 +0300

    add Test class

--- Collection/Iterator/src/main/java/ru/serg/PrimesValues.java
+++ Collection/Iterator/src/main/java/ru/serg/PrimesValues.java
@@ -1,8 +1,19 @@
 package ru.serg;
 
+import java.util.Iterator;
+
 /**
  * @author Sergey Miheev. Create on 17.08.2016
  * @version 1.0
  */
-public class PrimesValues {
+public class PrimesValues implements Iterator<Integer> {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Integer next() {
+        return null;
+    }
 }
