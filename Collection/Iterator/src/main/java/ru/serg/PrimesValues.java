19d32f87505d8ab0b5ff77bea81d1a410d27f034
mikheyevsv <mikheyevsv@gmail.com>
2016-08-23 16:19:47 +0300

    add test

--- Collection/Iterator/src/main/java/ru/serg/PrimesValues.java
+++ Collection/Iterator/src/main/java/ru/serg/PrimesValues.java
@@ -6,7 +6,15 @@ import java.util.Iterator;
  * @author Sergey Miheev. Create on 17.08.2016
  * @version 1.0
  */
-public class PrimesValues implements Iterator {
+public class PrimesValues implements Iterator <Integer>{
+
+    private int index = 0;
+    private int [] value;
+
+    public PrimesValues(int[] value) {
+        this.value = value;
+    }
+
     @Override
     public boolean hasNext() {
         return false;
--- Collection/Iterator/src/test/java/ru/serg/PrimesValuesTest.java
+++ Collection/Iterator/src/test/java/ru/serg/PrimesValuesTest.java
@@ -1,9 +1,29 @@
 package ru.serg;
 
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 /**
  * @author Sergey Miheev. Create on 17.08.2016
  * @version 1.0
  */
 public class PrimesValuesTest {
 
+    @Test
+    public void whenPassesThroughArrayIteratorMovesForward() {
+
+        PrimesValues iterator = new PrimesValues(new int[]{1, 2, 3});
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        boolean res = iterator.hasNext();
+
+        assertThat(res, is(false));
+
+    }
+
+    @Test
+    public void whenIteratorMovedThroughArrayShouldReturnCorrectValue(){
+        PrimesValues iterator = new PrimesValues(new int[]{1, 2, 3, 4});
+    }
 }
