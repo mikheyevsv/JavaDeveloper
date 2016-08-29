a84c3166af34e5befc139d6196d9bec520fc05f6
mikheyevsv <mikheyevsv@gmail.com>
2016-08-29 14:41:00 +0300

    Convert

---
+++ Collection/Iterator/src/main/java/ru/serg/ConvertIterator.java
@@ -0,0 +1,19 @@
+package ru.serg;
+
+import java.util.Iterator;
+
+/**
+ * @author Sergey Miheev. Create on 29.08.2016
+ * @version 1.0
+ */
+public class ConvertIterator implements Iterator {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+}
